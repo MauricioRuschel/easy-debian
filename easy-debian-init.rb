06b3b581a44a0243f61b766177dfc3c888002ae0
Douglas Quintiliano dos Santos <douglas.q.santo@gmail.com>
2015-12-30 21:41:55 -0200

    Added support to set up proxy, created a new module to handle the http requests, Core was surrounded with a module

--- easy-debian-init.rb
+++ easy-debian-init.rb
@@ -26,219 +26,293 @@
 #License: MIT license http://opensource.org/licenses/MIT
 #--------------------------------------------------------------------------
 
-# Used to download files
-require 'open-uri'
+# Core library from Ruby
+require 'rubygems'
 
 # Used as extension of file library
 require 'fileutils'
 
+# Used to work with http protocol
+require 'net/http'
+require 'open-uri'
+
+
 # Module that controls the colors used
 module Tty extend self
-def black; bold 30; end
-def red; bold 31; end
-def green; bold 32; end
-def yellow; bold 33; end
-def blue; bold 34; end
-def purple; bold 35; end
-def cyan; bold 36; end
-def white; bold 37; end
-def grey; bold 38; end
-def reset; escape 0; end
-def bold n; escape "1;#{n}" end
-def underline n; escape "4;#{n}" end
-def escape n; "\033[#{n}m" if STDOUT.tty? end
+
+  def black; bold 30; end
+  def red; bold 31; end
+  def green; bold 32; end
+  def yellow; bold 33; end
+  def blue; bold 34; end
+  def purple; bold 35; end
+  def cyan; bold 36; end
+  def white; bold 39; end
+  def grey; bold 38; end
+  def reset; escape 0; end
+  def bold n; escape "1;#{n}" end
+  def underline n; escape "4;#{n}" end
+  def escape n; "\033[#{n}m" if STDOUT.tty? end
+
+end
+
+# Module to handle http requests
+module Crawler
+  # Usage examples:
+  # Without proxy
+  #c = Crawler::NetHttp.new()
+  # Using only server and port for proxy connection
+  #c = Crawler::NetHttp.new("10.0.0.254", "3128")
+  # Using server, port, user and password for proxy connection
+  #c = Crawler::NetHttp.new("<http proxy URL>", "<port >", "Proxy user name", "Proxy Password")
+  class NetHttp
+    def initialize(proxy_host=nil, proxy_port=nil, proxy_user = nil, proxy_pass = nil)
+      @proxy_host =  proxy_host;
+      @proxy_port =  proxy_port;
+      @proxy_user =  proxy_user;
+      @proxy_pass =  proxy_pass;
+    end
+
+    def request_response(uri_str, limit = 10)
+      begin
+        http = Net::HTTP::Proxy(@proxy_host, @proxy_port, @proxy_user, @proxy_pass)
+        result = http.get_response(URI.parse(uri_str))
+        case result
+          when Net::HTTPSuccess
+          then  puts "#{Tty.blue}The file: #{Tty.white}#{uri_str}#{Tty.blue} was downloaded and saved! #{Tty.reset}"
+          result
+          when Net::HTTPRedirection then request_response(result['location'], limit - 1)
+          else
+            result.error!
+        end
+      rescue Exception => e
+          puts "#{Tty.red} #{e.message} #{Tty.white}#{uri_str} #{Tty.reset}"
+          return false
+      end
+    end
+  end
 end
 
 
-class Core
+module EasyDebian
+  # Usage examples:
+  # Usage examples:
+  # Without proxy
+  # new_deb = EasyDebian::Core.new()
+  # Using only server and port for proxy connection
+  # new_deb = EasyDebian::Core.new("10.0.0.254", "3128")
+  # Using server, port, user and password for proxy connection
+  # new_deb = EasyDebian::Core.new("<http proxy URL>", "<port >", "Proxy user name", "Proxy Password")
 
-  # Initialize the class and check if the script is running as root user
-  def initialize
-    abort "#{Tty.red}Needs to run this as root!#{Tty.reset}" if Process.uid != 0
-    puts "#{Tty.blue}This script is running with the following PID: #{Tty.white}"  + Process.pid.to_s + "#{Tty.reset}"
-  end
+  class Core
 
-  # Method to call the sleep command and wait for a given seconds
-  def sleep_time(time)
-    system "sleep #{time}s"
-  end
+    def initialize(proxy_host=nil, proxy_port=nil, proxy_user = nil, proxy_pass = nil)
+      @proxy_host =  proxy_host;
+      @proxy_port =  proxy_port;
+      @proxy_user =  proxy_user;
+      @proxy_pass =  proxy_pass;
 
-  # Method to install packages
-  # TODO: needs to validate if the mode is interactive or not
-  def install_packages(packages,comment)
-    aptitude = '/usr/bin/aptitude'
-    no_interactive = 'DEBIAN_FRONTEND=noninteractive'
-
-    puts "#{Tty.blue}Updating Repositories #{Tty.reset}"
-    update_repo = system "#{aptitude} update"
-    sleep_time(2)
-
-    if update_repo
-      puts "#{Tty.blue}Repositories Updated! #{Tty.reset}"
-    else
-      puts "#{Tty.red}Repositories Not Updated! #{Tty.reset}"
+      # Check if the script is running as root user
+      abort "#{Tty.red}Needs to run this as root!#{Tty.reset}" if Process.uid != 0
+      puts "#{Tty.blue}This script is running with the following PID: #{Tty.white}"  + Process.pid.to_s + "#{Tty.reset}"
     end
 
-    puts "#{Tty.blue}Installing Packages: #{Tty.white}#{comment} #{Tty.reset}"
-    install_pkgs = system "#{no_interactive}; #{aptitude} install #{packages} -y"
-    sleep_time(2)
+    # Method to call the sleep command and wait for a given seconds
+    def sleep_time(time)
+      system "sleep #{time}s"
+    end
 
-    if install_pkgs
-      puts "#{Tty.blue}Packages: #{Tty.white}#{comment} #{Tty.blue}Installed or already Installed ! #{Tty.reset}"
-    else
-      puts "#{Tty.red}Packages Not Installed! #{Tty.reset}"
+    # Method to install packages
+    # TODO: needs to validate if the mode is interactive or not
+    def install_packages(packages,comment)
+      aptitude = '/usr/bin/aptitude'
+      no_interactive = 'DEBIAN_FRONTEND=noninteractive'
+
+      puts "#{Tty.blue}Updating Repositories #{Tty.reset}"
+      update_repo = system "#{aptitude} update"
       sleep_time(2)
-    end
-  end
 
-  # Method to execute a command and return its result
-  # Used to get system information such as architecture and another stuffs
-  def exec_command(command)
-    output = `#{command}`.chomp
-    result=$?.success?
-    unless result
-      puts "#{Tty.red}Command with some problem! #{Tty.reset}"
-    end
-    # If no problems with the command line return
-    return output
-  end
-
-  # Method get remote files from the github so far, but it will be extended
-  # in the future.
-  def download_files(path,url)
-    # Check if the url is available
-    result = open("#{url}", 'User-Agent' => "Ruby/#{RUBY_VERSION}")
-    check_file_exits(path)
-    if result.status[1] == 'OK'
-      # Gets the file and save it.
-      File.open("#{path}", 'w') do |saved_file|
-        # the following "open" is provided by open-uri
-        open("#{url}", 'User-Agent' => "Ruby/#{RUBY_VERSION}") do |read_file|
-          puts "#{Tty.blue}The file: #{Tty.white}#{path}#{Tty.blue} was downloaded and saved! #{Tty.reset}" if saved_file.write(read_file.read)
-        end
+      if update_repo
+        puts "#{Tty.blue}Repositories Updated! #{Tty.reset}"
+      else
+        puts "#{Tty.red}Repositories Not Updated! #{Tty.reset}"
       end
-    else
-      puts "#{Tty.red}The url does not exists or is Unavailable! #{Tty.reset}"
-    end
-  end
 
-  # Method to create automatically backup of the files before overwrite them.
-  def check_file_exits(file)
-    if File.exists?(file)
-      check_mv = FileUtils.move(file,"#{file}.bkp")
-      check_status(check_mv,"Creating a backup of file: #{Tty.white} #{file} #{Tty.reset}")
-    end
-  end
-
-  # Method to create automatically backup of the files before overwrite them.
-  def check_dir_exits(dir)
-    if Dir.exists?(dir)
-      check_mv = FileUtils.move(dir,"#{dir}.bkp")
-      check_status(check_mv,"Creating a backup of directory: #{Tty.white} #{dir} #{Tty.reset}")
-    end
-  end
-
-  # Method to handle the keyrings, here we can update a lot of keys only sending
-  # as an array.
-  def conf_keyrings(url,keys=[])
-    gpg = '/usr/bin/gpg'
-    keys.each do |key|
-      import_key = system "#{gpg} --keyserver #{url} --recv-keys #{key}"
-      check_status(import_key,"Updating #{Tty.white}#{key}#{Tty.blue} in the Keyring")
+      puts "#{Tty.blue}Updating the System #{Tty.reset}"
+      update_system = system "#{aptitude} dist-upgrade -y"
       sleep_time(2)
+
+      if update_system
+        puts "#{Tty.blue}System Updated! #{Tty.reset}"
+      else
+        puts "#{Tty.red}System Not Updated! #{Tty.reset}"
+      end
+
+      puts "#{Tty.blue}Installing Packages: #{Tty.white}#{comment} #{Tty.reset}"
+      install_pkgs = system "#{no_interactive}; #{aptitude} install #{packages} -y"
+      sleep_time(2)
+
+      if install_pkgs
+        puts "#{Tty.blue}Packages: #{Tty.white}#{comment} #{Tty.blue}Installed! #{Tty.reset}"
+      else
+        puts "#{Tty.red}Packages Not Installed! #{Tty.reset}"
+        sleep_time(2)
+      end
     end
 
-    apt_key='/usr/bin/apt-key'
-    add_pubring = system "#{apt_key} add ~root/.gnupg/pubring.gpg"
-    check_status(add_pubring,'Adding the pubring')
-  end
-
-  # Method to convert the files to unix format, in some cases we need this feature
-  # so is better has a method to handle it.
-  def convert_file(files=[])
-    dos2unix = '/usr/bin/dos2unix -q'
-    files.each do |file|
-      convert = system("#{dos2unix} #{file}")
-      check_status(convert, "Converting the file: #{Tty.white}#{file}#{Tty.blue}")
+    # Method to execute a command and return its result
+    # Used to get system information such as architecture and another stuffs
+    def exec_command(command)
+      output = `#{command}`.chomp
+      result=$?.success?
+      unless result
+        puts "#{Tty.red}Command with some problem! #{Tty.reset}"
+      end
+      # If no problems with the command line return
+      return output
     end
-  end
 
-  # Method to configure the crontab
-  def conf_crontab(path)
-    crontab = '/usr/bin/crontab'
-    bkp_crontab = system("#{crontab} -l > crontab.bkp")
-    check_status(bkp_crontab, "Creating backup of #{Tty.white}crontab#{Tty.blue} file")
-    remove_crontab = system("#{crontab} -r")
-    check_status(remove_crontab, "Removing the #{Tty.white}crontab#{Tty.blue} configuration")
-    add_crontab = system("#{crontab} #{path}")
-    check_status(add_crontab, "Adding the new #{Tty.white}crontab#{Tty.blue} configuration")
-  end
+    # Method get remote files from the github so far, but it will be extended
+    # in the future.
+    def download_files(path,url)
+      c = Crawler::NetHttp.new(@proxy_host, @proxy_port, @proxy_user, @proxy_pass)
+      result = c.request_response("#{url}")
+      if result
+        File.open("#{path}", 'w') { |file| file.write(result.body) }
+      end
+    end
 
-  # Method to get git repositories
-  def get_git(path,url,comment,copy)
-    git = '/usr/bin/git'
-    cp = '/bin/cp'
-    check_dir_exits(path)
-    check_git = system("#{git} clone #{url} #{path}")
-    check_status(check_git, "Cloning the #{Tty.white}#{comment}#{Tty.reset}")
-    if !copy.empty?
-        if Dir.exists?(copy)
-          copy_repo = system("#{cp} -Rfa #{path}/* #{copy}")
-          check_status(copy_repo, "Copying the #{Tty.white}#{comment} #{Tty.blue}to #{Tty.white} #{copy} ")
-        else
-          puts "#{Tty.red}The directory #{Tty.white}#{copy} #{Tty.red}does not exists!#{Tty.reset}"
+    # Method to create automatically backup of the files before overwrite them.
+    def check_file_exits(file)
+      if File.exists?(file)
+        check_mv = FileUtils.move(file,"#{file}.bkp")
+        check_status(check_mv,"Creating a backup of file: #{Tty.white} #{file} #{Tty.reset}")
+      end
+    end
+
+    # Method to create automatically backup of the files before overwrite them.
+    def check_dir_exits(dir)
+      if Dir.exists?(dir)
+        if Dir.exists?("#{dir}.bkp")
+          FileUtils.remove_dir("#{dir}.bkp")
         end
+        check_mv = FileUtils.move(dir,"#{dir}.bkp")
+        check_status(check_mv,"Creating a backup of directory: #{Tty.white} #{dir} #{Tty.reset}")
+      end
     end
-  end
 
-  # Method to get the OS version, it will be used for the another scripts
-  def check_os_version(os)
-    cat = '/bin/cat'
-    debian_version = '/etc/debian_version'
-    centos_version = '/etc/redhat-relase'
+    # Method to handle the keyrings, here we can update a lot of keys only sending
+    # as an array.
+    def conf_keyrings(url,keys=[])
+      gpg = '/usr/bin/gpg'
+      keys.each do |key|
+        import_key = system "#{gpg} --keyserver #{url} --recv-keys #{key}"
+        check_status(import_key,"Updating #{Tty.white}GPG KEY: #{key}")
+        sleep_time(2)
+      end
 
-    os_availables = %w(debian centos)
-    if os_availables.include?(os.downcase)
-      if os == 'debian'
-        if File.exists?(debian_version)
-          os_version = `#{cat} #{debian_version}`.chomp[/\d+/]
-          case os_version
-            when '6'
-              return os_version_name = 'squeeze'
-            when '7'
-              return os_version_name = 'wheezy'
-            when '8'
-              return os_version_name = 'jessie'
+      apt_key='/usr/bin/apt-key'
+      add_pubring = system "#{apt_key} add ~root/.gnupg/pubring.gpg"
+      check_status(add_pubring,"Adding the #{Tty.white}Pubring")
+    end
+
+    # Method to convert the files to unix format, in some cases we need this feature
+    # so is better has a method to handle it.
+    def convert_file(files=[])
+      dos2unix = '/usr/bin/dos2unix -q'
+      files.each do |file|
+        convert = system("#{dos2unix} #{file}")
+        check_status(convert, "Converting the file: #{Tty.white}#{file}#{Tty.blue}")
+      end
+    end
+
+    # Method to configure the crontab
+    def conf_crontab(path)
+      crontab = '/usr/bin/crontab'
+      bkp_crontab = system("#{crontab} -l > crontab.bkp")
+      check_status(bkp_crontab, "Creating backup of #{Tty.white}crontab#{Tty.blue} file")
+      remove_crontab = system("#{crontab} -r")
+      check_status(remove_crontab, "Removing the #{Tty.white}crontab#{Tty.blue} configuration")
+      add_crontab = system("#{crontab} #{path}")
+      check_status(add_crontab, "Adding the new #{Tty.white}crontab#{Tty.blue} configuration")
+    end
+
+    # Method to get git repositories
+    def get_git(path,url,comment,copy)
+      git = '/usr/bin/git'
+      cp = '/bin/cp'
+      check_dir_exits(path)
+      check_git = system("#{git} clone #{url} #{path}")
+      check_status(check_git, "Cloning the #{Tty.white}#{comment}#{Tty.reset}")
+
+      if Dir.exists?(path)
+        if !copy.empty?
+            if Dir.exists?(copy)
+              copy_repo = system("#{cp} -Rfa #{path}/* #{copy}")
+              check_status(copy_repo, "Copying the #{Tty.white}#{comment} #{Tty.blue}to#{Tty.white}#{copy} ")
             else
-              puts "#{Tty.red}The Debian given does not have support yet! #{Tty.reset}"
-          end
-        else
-          puts "#{Tty.red}The #{Tty.white}#{debian_version}#{Tty.red} does not exists! #{Tty.reset}"
+              puts "#{Tty.red}The directory #{Tty.white}#{copy} #{Tty.red}does not exists!#{Tty.reset}"
+            end
         end
-      elsif os == 'centos'
-        if File.exists?(centos_version)
-          os_version_name = `#{cat} #{centos_version}`.chomp[/\d+/]
-        else
-          puts "#{Tty.red}The #{Tty.white}#{centos_version}#{Tty.red} does not exists! #{Tty.reset}"
-        end
+      else
+        puts "#{Tty.red}The directory #{Tty.white}#{path} #{Tty.red}does not exists!#{Tty.reset}"
       end
-    else
-      puts "#{Tty.red}The OS given does not have support yet! #{Tty.reset}"
+
     end
-    return os_version_name
+
+    # Method to get the OS version, it will be used for the another scripts
+    def check_os_version(os)
+      cat = '/bin/cat'
+      debian_version = '/etc/debian_version'
+      centos_version = '/etc/redhat-relase'
+
+      os_availables = %w(debian centos)
+      if os_availables.include?(os.downcase)
+        if os == 'debian'
+          if File.exists?(debian_version)
+            os_version = `#{cat} #{debian_version}`.chomp[/\d+/]
+            case os_version
+              when '6'
+                return os_version_name = 'squeeze'
+              when '7'
+                return os_version_name = 'wheezy'
+              when '8'
+                return os_version_name = 'jessie'
+              else
+                puts "#{Tty.red}The Debian given does not have support yet! #{Tty.reset}"
+            end
+          else
+            puts "#{Tty.red}The #{Tty.white}#{debian_version}#{Tty.red} does not exists! #{Tty.reset}"
+          end
+        elsif os == 'centos'
+          if File.exists?(centos_version)
+            os_version_name = `#{cat} #{centos_version}`.chomp[/\d+/]
+          else
+            puts "#{Tty.red}The #{Tty.white}#{centos_version}#{Tty.red} does not exists! #{Tty.reset}"
+          end
+        end
+      else
+        puts "#{Tty.red}The OS given does not have support yet! #{Tty.reset}"
+      end
+      return os_version_name
+    end
+
+    # Private methods that can be called only inside the Class
+    private
+
+    # Method to check the status of the commands
+    def check_status(check,message)
+      if check
+        puts "#{Tty.blue}#{message} #{Tty.blue}executed successfully! #{Tty.reset}"
+      else
+        puts "#{Tty.red}#{message} #{Tty.red}executed with errors! #{Tty.reset}"
+      end
+    end
+
+  ### END CLASS
   end
 
-  # Private methods that can be called only inside the Class
-  private
-
-  # Method to check the status of the commands
-  def check_status(check,message)
-    if check
-      puts "#{Tty.blue}#{message} #{Tty.blue}executed successfully! #{Tty.reset}"
-    else
-      puts "#{Tty.red}#{message} #{Tty.red}executed with errors! #{Tty.reset}"
-    end
-  end
+### END MODULE
 end
 
 ### END OF THE CORE CLASS
@@ -247,7 +321,14 @@ end
 os_version = 'debian'
 
 # Creating a new object to use the methods
-new_deb = Core.new
+# Usage examples:
+# Without proxy
+# new_deb = EasyDebian::Core.new()
+# Using only server and port for proxy connection
+# new_deb = EasyDebian::Core.new("10.0.0.254", "3128")
+# Using server, port, user and password for proxy connection
+# new_deb = EasyDebian::Core.new("<http proxy URL>", "<port >", "Proxy user name", "Proxy Password")
+new_deb = EasyDebian::Core.new()
 
 # Check the os Version
 os_version_name = new_deb.check_os_version("#{os_version}")
